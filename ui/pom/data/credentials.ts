7d5bb2f60d375fff09e6d44badd74be1ffd4350c
Yoany Mora <yoany.stephany@gmail.com>
2026-03-20 11:36:12 -0600

    Modify project target page and add base code:
    - Create PageFactory and LoginPage classes
    - Create seed to be used with playwright agents
    - Create fixture for login with correct credentials
    - Modify `testIdAttribute` attribute inside the defineConfig object, this impacts the page.getByTestId() locator
    - Add some constants to be used across the framework

---
+++ ui/pom/data/credentials.ts
@@ -0,0 +1,4 @@
+export const LOGIN_CREDENTIALS = {
+	STANDARD_USER: process.env.STANDARD_USER || "default user",
+	PASSWORD: process.env.PASSWORD || "default password",
+};
---
+++ ui/pom/data/urls.ts
@@ -0,0 +1,2 @@
+export const URL_BASE = "https://automationexercise.com/";
+export const URL_LOGIN = `${URL_BASE}/login`;
