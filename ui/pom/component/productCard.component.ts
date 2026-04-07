044640e06a999b0d52203a5ad6e9075c1b0a8432
Francisco Mondragón <fjgm11@hotmail.com>
2026-04-07 10:30:09 -0600

    squashing

--- ui/pom/component/productCard.component.ts
+++ ui/pom/component/productCard.component.ts
@@ -5,6 +5,7 @@ export class ProductCard {
 	readonly productName: Locator;
 	readonly productPrice: Locator;
 	readonly addToCartButton: Locator;
+	readonly addtoCartOverlayButton: Locator;
 	readonly viewProductButton: Locator;
 
 	constructor(root: Locator) {
@@ -12,11 +13,12 @@ export class ProductCard {
 		this.productName = root.locator(".single-products>div>p");
 		this.productPrice = root.locator(".single-products>div>h2");
 		this.addToCartButton = root.locator(".productinfo .add-to-cart");
+		this.addtoCartOverlayButton = root.locator(".productinfo .add-to-cart");
 		this.viewProductButton = root.locator(".choose a");
 	}
 
 	async addToCart() {
-		await this.addToCartButton.click();
+		await this.addtoCartOverlayButton.click();
 	}
 
 	async viewProduct() {
