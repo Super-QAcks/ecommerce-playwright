914565cd478fd54e028639bcf4ac2ceb599f1544
Mike Sanchez <mike.sanchez.me@gmail.com>
2026-05-21 16:19:38 -0600

    Test Case 08: Verify All Products and product detail page

---
+++ ui/tests/products.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from "@playwright/test";
+import { HomePage } from "../pom/pages/homePage";
+import { Products } from "../pom/pages/productsPage";
+import { HeaderComponent } from "../pom/component/header.component";
+import { URL_BASE } from "../pom/data/urls";
+import { ProductDetailsPage } from "../pom/pages/productDetailsPage";
+
+test.describe("Products Page Tests", () => {
+	let homePage: HomePage;
+	let productsPage: Products;
+	let productDetailsPage: ProductDetailsPage;
+	test.beforeEach(async ({ page }) => {
+		homePage = new HomePage(page);
+		productsPage = new Products(page);
+		productDetailsPage = new ProductDetailsPage(page);
+	});
+
+	test("Test Case 8: Verify All products and product details page", async ({
+		page,
+	}) => {
+		await test.step("Navigate to url", async () => {
+			await homePage.goto(URL_BASE);
+		});
+
+		await test.step("Verify that home page is visible successfully", async () => {
+			await homePage.waitForRoot();
+		});
+
+		await test.step("Click on Products page button", async () => {
+			await homePage.clickProductsButton();
+		});
+
+		await test.step("Verify that user navigated to Products page successfully", async () => {
+			await productsPage.validateProductsPage();
+		});
+
+		await test.step("Verify that products list is visible", async () => {
+			await productsPage.validateProductsList();
+		});
+
+		await test.step("Click on first product's 'View Product' button", async () => {
+			await productsPage.clickViewFirstProductButton();
+		});
+
+		await test.step("Verify that user navigated to product details page successfully", async () => {
+			await productDetailsPage.waitForRoot();
+		});
+
+		await test.step("Verify that product details are visible", async () => {
+			await productDetailsPage.validateProductDetails();
+		});
+	});
+});
