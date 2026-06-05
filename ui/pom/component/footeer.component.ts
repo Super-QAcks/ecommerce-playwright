b6383512453731e9503458e8425b3c4106bb9a9e
Francisco Mondragón <fjgm11@hotmail.com>
2026-06-05 14:59:16 -0600

    test #11

--- ui/pom/component/footeer.component.ts
+++ ui/pom/component/footeer.component.ts
@@ -3,9 +3,29 @@ import { Page, Locator } from "@playwright/test";
 export class FooterComponent {
 	readonly footerRoot: Locator;
 	readonly titleWidget: Locator;
+	readonly susbcriptionTitle: Locator;
+	readonly subscriptionInput: Locator;
+	readonly subscriptionButton: Locator;
+	readonly successSubscribeMessage: Locator;
 
 	constructor(page: Page) {
 		this.footerRoot = page.locator("#footer");
 		this.titleWidget = page.locator(".single-widget");
+		this.susbcriptionTitle = page.getByRole("heading", {
+			name: "Subscription",
+		});
+		this.subscriptionInput = page.getByRole("textbox", {
+			name: "Your email address",
+		});
+		this.subscriptionButton = page.locator("#subscribe");
+		this.successSubscribeMessage = page.locator("#success-subscribe");
+	}
+
+	async fillSubscription(email: string) {
+		await this.subscriptionInput.fill(email);
+	}
+
+	async clickSubscriptionButton() {
+		await this.subscriptionButton.click();
 	}
 }
--- ui/tests/cart.spec.ts
+++ ui/tests/cart.spec.ts
@@ -3,9 +3,51 @@ import { VALID_PRODUCTS } from "../pom/data/products";
 import { URL_BASE } from "../pom/data/urls";
 import { HomePage } from "../pom/pages/homePage";
 import { CartPage } from "../pom/pages/CartPage";
+import { HeaderComponent } from "../pom/component/header.component";
+import { FooterComponent } from "../pom/component/footeer.component";
 import { AddedProductModal } from "../pom/component/addedProductModal.component";
+import { USER_SIGNUP } from "../pom/data/credentials";
 
 test.describe("Cart Test Cases", async () => {
+	test("Test Case 11: Verify Subscription in Cart page", async ({ page }) => {
+		const homePage = new HomePage(page);
+		const cartPage = new CartPage(page);
+		const headerComponent = new HeaderComponent(page);
+		const footerComponent = new FooterComponent(page);
+
+		await test.step("Launch browser and navigate to url", async () => {
+			await homePage.goto(URL_BASE);
+		});
+
+		await test.step("Verify that home page is visible successfully", async () => {
+			await homePage.waitForRoot();
+		});
+
+		await test.step("Click 'Cart' button", async () => {
+			await headerComponent.clickCart();
+			await cartPage.waitForCart();
+		});
+
+		await test.step("Scroll down to footer", async () => {
+			await page.evaluate(() => {
+				window.scrollTo(0, document.body.scrollHeight);
+			});
+		});
+
+		await test.step("Verify text 'SUBSCRIPTION", async () => {
+			await expect(footerComponent.susbcriptionTitle).toBeVisible();
+		});
+
+		await test.step("Enter email address in input and click arrow button", async () => {
+			await footerComponent.fillSubscription(USER_SIGNUP.email);
+			await footerComponent.clickSubscriptionButton();
+		});
+
+		await test.step("Verify success message 'You have been successfully subscribed!' is visible", async () => {
+			await expect(footerComponent.successSubscribeMessage).toBeVisible();
+		});
+	});
+
 	test("Test Case 17: Remove Products From Cart", async ({ page }) => {
 		const homePage = new HomePage(page);
 		const cartPage = new CartPage(page);
