a1f811c0e239883af956a109fda5e9db5e06a0ca
Francisco Mondragón <fjgm11@hotmail.com>
2026-06-05 17:35:16 -0600

    comments removed

--- ui/tests/productsPage.spec.ts
+++ ui/tests/productsPage.spec.ts
@@ -10,39 +10,32 @@ test("Test Case 09: Search Product", async ({ page }) => {
 	const headerComponent = new HeaderComponent(page);
 	const productsPage = new ProductsPage(page);
 
-	// 1. Launch browser
-	// 2. Navigate to url 'http://automationexercise.com'
 	await test.step("Launch browser and Navigate to url 'http://automationexercise.com'", async () => {
 		await homePage.goto(URL_BASE);
 	});
-	// 3. Verify that home page is visible successfully
+
 	await test.step("Verify that home page is visible successfully", async () => {
 		await expect(homePage.validateHomePage()).toBeTruthy();
 	});
 
-	// 4. Click on 'Products' button
 	await test.step("Click on 'Products' button", async () => {
 		await headerComponent.clickProductsLink();
 	});
 
-	// 5. Verify user is navigated to ALL PRODUCTS page successfully
 	await test.step("Verify user is navigated to ALL PRODUCTS page successfully", async () => {
 		await productsPage.waitForProductsPage();
 	});
 
-	// 6. Enter product name in search input and click search button
 	await test.step("Enter product name in search input and click search button", async () => {
 		await productsPage.fillSearchProduct(PRODUCT_DETAILS.default.name);
 		await productsPage.clickSearchButton();
 	});
 
-	// 7. Verify 'SEARCHED PRODUCTS' is visible
 	await test.step("Verify 'SEARCHED PRODUCTS' is visible", async () => {
 		await productsPage.waitForSearchedProductsHeading();
 		await expect(productsPage.searchedProductsHeading).toBeVisible();
 	});
 
-	// 8. Verify all the products related to search are visible
 	await test.step("Verify all the products related to search are visible", async () => {
 		const searchWord = PRODUCT_DETAILS.default.name;
 		const resultCards =
