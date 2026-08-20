6812f301b9543fb90682626ddfb3ae1d6ffccce0
PC Mondro <fjgm11@hotmail.com>
2026-08-20 17:12:16 -0600

    fix branch with rebase

--- ui/pom/pages/homePage.ts
+++ ui/pom/pages/homePage.ts
@@ -30,6 +30,11 @@ export class HomePage extends PageFactory {
 	}
 
 	async waitForRoot() {
+		await this.homeSlider.waitFor({ state: "visible" });
+	}
+
+	async validateHomePage() {
+		await this.waitForRoot();
 		await this.homeSlider.isVisible();
 	}
 
