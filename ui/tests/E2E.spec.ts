e8d4d6c70ba6277e098131c7b352e9fe9a43bda2
PC Mondro <fjgm11@hotmail.com>
2026-09-04 18:16:13 -0600

    Added test 24 download invoice

--- ui/tests/E2E.spec.ts
+++ ui/tests/E2E.spec.ts
@@ -253,7 +253,134 @@ test.describe("E2E Tests", () => {
 			await header.clickDeleteAccount();
 		});
 
-		await test.step("", async () => {
+		await test.step("Verify 'ACCOUNT DELETED!' and click 'Continue' button", async () => {
+			await deletedAccountPage.waitForRoot();
+			await expect(deletedAccountPage.accountDeletedTitle).toContainText(
+				"Account Deleted!"
+			);
+			await deletedAccountPage.clickContinue();
+		});
+	});
+
+	test("Test Case 24: Download Invoice after purchase order", async ({
+		page,
+	}) => {
+		header = new HeaderComponent(page);
+		loginPage = new LoginPage(page);
+		signUpPage = new SignUpPage(page);
+		accounCreatedPage = new AccountCreatedPage(page);
+		checkoutPage = new CheckoutPage(page);
+		paymentPage = new PaymentPage(page);
+		paymentDonePage = new PaymentDonePage(page);
+		deletedAccountPage = new DeletedAccountPage(page);
+
+		await test.step("Navigate to url", async () => {
+			await homePage.goto(URL_BASE);
+		});
+
+		await test.step("Verify that home page is visible successfully", async () => {
+			await homePage.waitForRoot();
+		});
+
+		await test.step("Add products to cart", async () => {
+			await homePage.addProductsToCart(VALID_PRODUCTS);
+		});
+
+		await test.step("Click 'Cart' button", async () => {
+			await homePage.clickScrollUp();
+			//await homePage.closeAdds();
+			await header.clickCart();
+		});
+
+		await test.step("Verify that cart page is displayed", async () => {
+			await cartPage.waitForCart();
+		});
+
+		await test.step("Click Proceed To Checkout", async () => {
+			await cartPage.clickCheckout();
+		});
+
+		await test.step("Click 'Register / Login' button", async () => {
+			await cartPage.waitForModalCheckout();
+			await cartPage.clickModalRegisterLogin();
+		});
+
+		await test.step("Fill all details in Signup and create account", async () => {
+			await loginPage.waitForSignUpRoot();
+			await loginPage.signUp(USER_SIGNUP.name, USER_SIGNUP.email);
+			await signUpPage.waitForRoot();
+			await signUpPage.fillForm(USER_SIGNUP);
+		});
+
+		await test.step("Verify 'ACCOUNT CREATED!' and click 'Continue' button", async () => {
+			await accounCreatedPage.waitForRoot();
+			await expect(accounCreatedPage.successMessage).toContainText(
+				"Account Created!"
+			);
+			await accounCreatedPage.clickContinueButton();
+		});
+
+		await test.step("Verify ' Logged in as username' at top", async () => {
+			await expect(header.loggedUserName).toHaveText(USER_SIGNUP.name);
+		});
+
+		await test.step("Click 'Cart' button", async () => {
+			await header.clickCart();
+		});
+
+		await test.step("Click 'Proceed To Checkout' button", async () => {
+			await cartPage.clickCheckout();
+		});
+
+		await test.step("Verify Address Details and Review Your Order", async () => {
+			await expect(checkoutPage.deliveryAddressForm).toContainText(
+				USER_SIGNUP.zipcode
+			);
+
+			await expect(checkoutPage.reviewOderSection).toContainText(
+				VALID_PRODUCTS.default.name
+			);
+		});
+
+		await test.step("Enter description in comment text area and click 'Place Order'", async () => {
+			await checkoutPage.commentOrder("Please deliver between 9 AM to 5 PM");
+			await checkoutPage.placeOrder();
+		});
+
+		await test.step("Enter payment details: Name on Card, Card Number, CVC, Expiration date", async () => {
+			await paymentPage.waitForRoot();
+			await paymentPage.enterCardDetails(CARD_DETAILS);
+		});
+
+		await test.step("Click 'Pay and Confirm Order' button", async () => {
+			await paymentPage.payAndConfirmOrder();
+		});
+
+		await test.step("Verify success message 'Your order has been placed successfully!'", async () => {
+			await paymentDonePage.waitForRoot();
+			await expect(paymentDonePage.successMessage).toContainText(
+				"Congratulations! Your order has been confirmed!"
+			);
+		});
+
+		const downloadPromise = page.waitForEvent("download");
+
+		await test.step("Click 'Download Invoice' button and verify invoice is downloaded successfully.", async () => {
+			await paymentDonePage.clickDownloadInvoice();
+			const download = await downloadPromise;
+			const path = await download.path();
+			expect(path).not.toBeNull();
+		});
+
+		await test.step("Click 'Continue' button", async () => {
+			await paymentDonePage.clickContinue();
+		});
+
+		await test.step("Click 'Delete Account' button", async () => {
+			await header.clickDeleteAccount();
+		});
+
+		await test.step("Verify 'ACCOUNT DELETED!' and click 'Continue' button", async () => {
 			await deletedAccountPage.waitForRoot();
 			await expect(deletedAccountPage.accountDeletedTitle).toContainText(
 				"Account Deleted!"
