517022b8b1e78e2e77e79ae3ca0cfbb6e65faead
PC Mondro <fjgm11@hotmail.com>
2026-09-12 13:03:13 -0600

    Test case 12 Add Products in Cart

--- ui/pom/component/cartItemCard.component.ts
+++ ui/pom/component/cartItemCard.component.ts
@@ -1,10 +1,11 @@
 import { Locator } from "@playwright/test";
-
+import { parsePrice } from "../utils/price";
 export class CartItemCard {
 	readonly rootCard: Locator;
 	readonly productName: Locator;
 	readonly productPrice: Locator;
 	readonly productQuantity: Locator;
+	readonly productTotalPrice: Locator;
 	readonly deleteButton: Locator;
 
 	constructor(root: Locator) {
@@ -12,11 +13,29 @@ export class CartItemCard {
 		this.productName = root.locator(".cart_description h4 a");
 		this.productPrice = root.locator(".cart_price p");
 		this.productQuantity = root.locator(".cart_quantity");
+		this.productTotalPrice = root.locator(".cart_total_price");
 		this.deleteButton = root.locator(".cart_quantity_delete");
 	}
 
 	async deleteItem() {
 		await this.deleteButton.click();
 		await this.rootCard.waitFor({ state: "hidden" });
+	}
+
+	async getProductName() {
+		return (await this.productName.innerText()).trim();
+	}
+
+	async getProductPrice() {
+		return parsePrice((await this.productPrice.innerText()).trim());
+	}
+
+	async getProductQuantity() {
+		const text = await this.productQuantity.innerText();
+		return Number(text.trim());
+	}
+
+	async getTotalPrice() {
+		return parsePrice(await this.productTotalPrice.innerText());
 	}
 }
